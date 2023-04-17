9832c898a730efa84c771c997c4fb6857c8d1e67
andherpaulo <andher.dev@gmail.com>
2023-04-17 09:30:56 -0300

    ci: add deploy job and change environment variable name

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.REACT_APP_BACK_END_URL,
 });
 
 export default instance;
